684e0dcc3e342a8ffc6b9c1575355c05fc7d5767
Daniel Moore <dfmoore@gmail.com>
2014-10-26 15:40:01 -0700

    started Rscripts

---
+++ Rscript.R
@@ -0,0 +1,10 @@
+setwd("C:\\Users\\Daniel\\Documents\\GitHub\\NSFData_WomeninSTEM\\datatables")
+setwd("C:\\Users\\damoore\\Documents\\GitHub\\NSFData_WomeninSTEM\\datatables")
+
+library(plyr); library(lattice); library(data.table); library(ggplot2); library(xlsx); library(reshape2)
+
+bachmajorfields<-read.xlsx2("tab11.xls", sheetIndex=1, startRow=3, as.data.frame=TRUE, header=TRUE, colClasses=rep("numeric",12))
+
+bmfields_melted<-melt(bachmajorfields, id=c("Academic.year.ending", "All.fieldsa"), na.rm=TRUE)
+
+qplot(Academic.year.ending, value, data=bmfields_melted, color=variable)
