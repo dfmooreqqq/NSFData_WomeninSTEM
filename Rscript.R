153e81d595631e263835b42f8136eca783831b81
Daniel Moore <dfmoore@gmail.com>
2014-12-14 08:02:12 -0800

    updated loading of packages and data

--- Rscript.R
+++ Rscript.R
@@ -1,15 +1,17 @@
-setwd("C:\\Users\\Daniel\\Documents\\GitHub\\NSFData_WomeninSTEM\\datatables")
-setwd("C:\\Users\\damoore\\Documents\\GitHub\\NSFData_WomeninSTEM\\datatables")
+## Environment setup
+# Load packages.
+packages <- c("plyr","lattice","data.table","ggplot2","xlsx", "reshape2")
+packages <- lapply(packages, FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+        install.packages(x)
+        library(x, character.only = TRUE)
+    }
+})
 
-library(plyr)
-library(lattice)
-library(data.table)
-library(ggplot2)
-library(xlsx)
-library(reshape2)
+# Set working directory
+workingdir<-paste("C:\\Users", Sys.getenv("USERNAME"), "Documents\\GitHub\\NSFData_WomeninSTEM\\datatables", sep = "\\")
+setwd(workingdir)
 
 bachmajorfields<-read.xlsx2("tab11.xls", sheetIndex=1, startRow=3, as.data.frame=TRUE, header=TRUE, colClasses=rep("numeric",12))
-
 bmfields_melted<-melt(bachmajorfields, id=c("Academic.year.ending", "All.fieldsa"), na.rm=TRUE)
-
 qplot(Academic.year.ending, value, data=bmfields_melted, color=variable)
